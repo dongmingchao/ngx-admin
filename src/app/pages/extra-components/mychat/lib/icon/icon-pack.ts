c51e150c82745be7ffa9e201d6a84c3db0f5b592
dongmingchao <1657883517@qq.com>
2019-05-05 17:59:48 +0800

    fix lint

--- src/app/pages/extra-components/mychat/lib/icon/icon-pack.ts
+++ src/app/pages/extra-components/mychat/lib/icon/icon-pack.ts
@@ -1,26 +1,27 @@
-import { NbIcon } from './icon';
+import {NbIcon} from './icon';
 
 export interface NbIcons {
-  [key: string]: NbIcon | string;
+    [key: string]: NbIcon | string;
 }
 
-export enum NbIconPackType  {
-  SVG = 'svg',
-  FONT = 'font',
+export enum NbIconPackType {
+    SVG = 'svg',
+    FONT = 'font',
 }
 
 export interface NbIconPackParams {
-  packClass?: string,
-  [name: string]: any,
+    packClass?: string;
+
+    [name: string]: any;
 }
 
 export interface NbFontIconPackParams extends NbIconPackParams {
-  iconClassPrefix?: string,
+    iconClassPrefix?: string;
 }
 
 export interface NbIconPack {
-  name: string;
-  type: NbIconPackType;
-  icons: Map<string, NbIcon | string>;
-  params: NbIconPackParams,
+    name: string;
+    type: NbIconPackType;
+    icons: Map<string, NbIcon | string>;
+    params: NbIconPackParams;
 }
